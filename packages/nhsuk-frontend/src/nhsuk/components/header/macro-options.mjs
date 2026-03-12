aca6a32a29a2d35cc0c98beec26d40bee7610511
Colin Rotherham <work@colinr.com>
2026-03-12 00:21:15 +0000

    Add support for header `colour` Nunjucks option

--- packages/nhsuk-frontend/src/nhsuk/components/header/macro-options.mjs
+++ packages/nhsuk-frontend/src/nhsuk/components/header/macro-options.mjs
@@ -328,6 +328,12 @@ const options = {
     required: false,
     description:
       'HTML attributes (for example data attributes) to add to the header container.'
+  },
+  colour: {
+    type: 'string',
+    required: false,
+    description:
+      'Optional colour modifier for the header – for example, `"white"`.'
   }
 }
 
